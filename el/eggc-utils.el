dbc2c9e4e1413228673beb945e6f949a1c98c171
eguchi.ken <eguchi.ken@moneyforward.co.jp>
2020-05-31 23:31:08 +0900

    Add comments to eggc-util like MELPA package format

--- el/eggc-utils.el
+++ el/eggc-utils.el
@@ -1,3 +1,28 @@
+;;; eggc-utils.el --- eggc utility functions -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020 eggc
+
+;; Author: eggc <no.eggchicken@gmail.com>
+;; Keywords: lisp
+;; Version: 0.0.1
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
 (defun insert-current-date (&optional diff)
   "日にちをカレントバッファに出力します"
   (interactive "P")
@@ -53,3 +78,6 @@
       (while replacement
         (goto-char (point-min))
         (replace-regexp (pop replacement) (pop replacement))))))
+
+(provide 'eggc-utils)
+;;; eggc-utils.el ends here
